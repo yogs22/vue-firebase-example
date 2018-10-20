7b465fcfeac56200fbbe2e4303444e66b41ea045
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2018-10-20 09:31:05 +0700

    Update firebase config

--- src/config/db.js
+++ src/config/db.js
@@ -2,18 +2,9 @@ import VueFire from 'vuefire'
 import firebase from 'firebase'
 import Vue from 'vue'
 
+import { init } from './config'
+
 Vue.use(VueFire)
 
-let config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
-  };
-
-let init = firebase.initializeApp(config);
 export const db = init.database()
-
 export const itemsRef = db.ref('items')
