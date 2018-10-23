4b0a05bf0d1c46fa9f3efbff4157acc0eee68bc1
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2018-10-23 09:43:49 +0700

    Update firebase config

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase'
 
+// Edit your firebase config here
 let config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyDa6U0eeNH5J9_8Mkp_Vs50s9mGofG14Tw",
+    authDomain: "vue-firebase-tutorial-cc28e.firebaseapp.com",
+    databaseURL: "https://vue-firebase-tutorial-cc28e.firebaseio.com",
+    projectId: "vue-firebase-tutorial-cc28e",
+    storageBucket: "vue-firebase-tutorial-cc28e.appspot.com",
+    messagingSenderId: "266264128559"
   };
 
 export const init = firebase.initializeApp(config);
